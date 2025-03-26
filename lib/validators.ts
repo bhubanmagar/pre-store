2fb0d6ad65a601ad1192c26a0f948c0549b6d27f
smash3r <bhubanmagar79@gmail.com>
2025-03-27 00:24:07 +0545

    feat: SignIn and SignOut action addedd

--- lib/validators.ts
+++ lib/validators.ts
@@ -31,3 +31,10 @@ export const insertProductSchema = z.object({
   banner: z.string().nullable(),
   price: Currency,
 });
+
+// schema for signin user
+
+export const signInFormSchema = z.object({
+  email: z.string().email("Invalid Email adress"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
