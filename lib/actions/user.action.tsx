c83782bd829631a07e3c8bb74c8f6920cebb1a29
smash3r <bhubanmagar79@gmail.com>
2025-03-31 00:19:17 +0545

    feat: SignUp User action

--- lib/actions/user.action.tsx
+++ lib/actions/user.action.tsx
@@ -1,7 +1,9 @@
 "use server";
-import { signInFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { hashSync } from "bcrypt-ts";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { signInFormSchema, SignUpFromSchema } from "../validators";
 
 // signin the user credentials
 export async function SignInWithCredentials(
@@ -27,4 +29,41 @@ export async function SignInWithCredentials(
 // sign user out
 export async function signOutUser() {
   await signOut();
+}
+
+interface SignUpResponseProps {
+  success: boolean;
+  message: string;
+}
+
+// signup user action
+export async function signUpUser(
+  prevState: SignUpResponseProps,
+  formData: FormData
+): Promise<SignUpResponseProps> {
+  try {
+    const user = await SignUpFromSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+    const plainPassword = user.password;
+    user.password = hashSync(user.password, 10);
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+    await signIn("credentials", { email: user.email, password: plainPassword });
+    return { success: true, message: "User Registered Sucessfully!" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    } else {
+      return { success: false, message: "User wasn't Registered" };
+    }
+  }
 }
